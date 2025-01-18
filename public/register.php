7f9e58b48b61a8f27235a3e3ed4d8eae77caac34
dk7458 <57878603+dk7458@users.noreply.github.com>
2025-01-18 16:36:12 +0100

    modified:   public/register.php

--- public/register.php
+++ public/register.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require '../includes/db_connect.php';
 require '../includes/functions.php';
 
