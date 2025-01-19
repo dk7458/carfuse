7dadc5a8b58bfa619e92408a9b0815de81f80228
dk7458 <57878603+dk7458@users.noreply.github.com>
2025-01-19 17:09:53 +0100

    modified:   views/shared/navbar.php 	modified:   views/shared/navbar_admin.php 	modified:   views/shared/navbar_user.php

--- views/shared/navbar_user.php
+++ views/shared/navbar_user.php
@@ -7,7 +7,7 @@ $userRole = $_SESSION['user_role'] ?? null;
 <nav class="navbar navbar-expand-lg" style="background-color: #e8e8e8;">
     <div class="container">
         <a class="navbar-brand" href="/public/index.php">
-            <img src="/public/logo.png" alt="Carfuse Logo" style="height: 40px;">
+            <img src="/public/logo.png" alt="Carfuse Logo" style="height: 70%;">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
