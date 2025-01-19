896c3c2937774b7e4a6b73c56d23f3ccf694c9f2
dk7458 <57878603+dk7458@users.noreply.github.com>
2025-01-19 20:35:21 +0100

    b

--- views/shared/navbar_empty.php
+++ views/shared/navbar_empty.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #e8e8e8;">
     <div class="container">
         <a class="navbar-brand" href="/public/index.php">
-            <img src="/public/logo.png" alt="Carfuse Logo" style="height: 70%;">
+            <img src="/public/logo.png" alt="Carfuse Logo" style="height: 35px;">
         </a>
     </div>
 </nav>
