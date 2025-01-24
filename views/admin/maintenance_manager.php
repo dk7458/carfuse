5d381b9d5ff15702f36551eec436986d2ac3aa29
dk7458 <doriankrok@gmail.com>
2025-01-24 22:01:42 +0100

    new file:   functions/maintenance.php 	modified:   views/admin/maintenance_manager.php

--- views/admin/maintenance_manager.php
+++ views/admin/maintenance_manager.php
@@ -7,6 +7,7 @@ require_once '/home/u122931475/domains/carfuse.pl/public_html/config.php';
 require_once BASE_PATH . 'includes/session_middleware.php';
 require_once BASE_PATH . 'includes/db_connect.php';
 require_once BASE_PATH . 'functions/global.php';
+require_once BASE_PATH . 'functions/maintenance.php';
 
 enforceRole(['admin', 'super_admin']);
 
