4d34eff3cff6ff673649e9da69d06b868016b36d
dk7458 <doriankrok@gmail.com>
2025-02-02 12:34:34 +0100

    db

--- config/database.php
+++ config/database.php
@@ -8,17 +8,17 @@ return [
     'app_database' => [
         'host' => '127.0.0.1',
         'port' => 3306,
-        'database' => 'carfuse_app',
-        'username' => 'app_user',
-        'password' => 'secure_password',
+        'database' => 'u122931475_carfuse',
+        'username' => 'u122931475_user',
+        'password' => 'Japierdole1876',
         'charset' => 'utf8mb4',
     ],
     'secure_database' => [
         'host' => '127.0.0.1',
         'port' => 3306,
-        'database' => 'carfuse_secure',
-        'username' => 'secure_user',
-        'password' => 'secure_password',
+        'database' => 'u122931475_secure',
+        'username' => 'u122931475_admin',
+        'password' => 'Japierdole1876',
         'charset' => 'utf8mb4',
     ],
 ];
