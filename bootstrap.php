907c7a424684857ed9c09cd6a5e18319e2269404
dk7458 <doriankrok@gmail.com>
2025-02-03 22:48:15 +0100

    j

--- bootstrap.php
+++ bootstrap.php
@@ -26,7 +26,7 @@ use DocumentManager\Services\EncryptionService;
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Force Load Environment Variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // Debug Check: Print Loaded Environment Variables
