a325c59c988c8f80acbc6c908eb069ac0ad493c2
dk7458 <doriankrok@gmail.com>
2025-02-04 05:34:47 +0100

    i

--- public/index.php
+++ public/index.php
@@ -1,50 +1,49 @@
 <?php
 
-require_once __DIR__ . '/../bootstrap.php';
-use FastRoute\RouteCollector;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = require __DIR__ . '/../bootstrap.php';
-
-$dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
-    $routes = require __DIR__ . '/../routes/web.php';
-    foreach ($routes->getData() as $route) {
-        [$method, $uri, $handler] = $route;
-        $r->addRoute($method, $uri, $handler);
-    }
-});
+// Load Route Dispatcher
+$dispatcher = require __DIR__ . '/../routes/web.php';
 
 // Fetch method and URI
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-// Remove query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?')) {
-    $uri = substr($uri, 0, $pos);
-}
-$uri = rawurldecode($uri);
+// Strip query string (?foo=bar) and decode URI
+$uri = strtok($uri, '?');
 
+// Dispatch the request
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         http_response_code(404);
-        echo json_encode(['status' => 'error', 'message' => 'Not found']);
+        echo "404 Not Found";
         break;
 
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         http_response_code(405);
-        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+        echo "405 Method Not Allowed";
         break;
 
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
 
-        if (is_array($handler)) {
-            [$class, $method] = $handler;
-            $controller = $container[$class];
-            echo json_encode($controller->$method(...array_values($vars)));
-        } elseif (is_callable($handler)) {
-            echo json_encode($handler(...array_values($vars)));
+        if (is_callable($handler)) {
+            call_user_func_array($handler, $vars);
+        } elseif (is_array($handler) && count($handler) === 2) {
+            [$controller, $method] = $handler;
+            if (class_exists($controller) && method_exists($controller, $method)) {
+                $instance = new $controller();
+                call_user_func_array([$instance, $method], $vars);
+            } else {
+                http_response_code(500);
+                echo "500 Internal Server Error: Controller or Method Not Found";
+            }
+        } else {
+            http_response_code(500);
+            echo "500 Internal Server Error: Invalid Route Handler";
         }
         break;
 }
