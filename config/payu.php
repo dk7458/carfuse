0c7698795d327f626aed3410f218839c45708132
dk7458 <doriankrok@gmail.com>
2025-02-04 05:11:24 +0100

    h

--- config/payu.php
+++ config/payu.php
@@ -3,4 +3,6 @@ return [
     'api_key' => 'your-payu-api-key',
     'api_secret' => 'your-payu-api-secret',
     'endpoint' => 'https://secure.payu.com/api',
+    'merchant_key' => 'your-merchant-key',
+    'merchant_salt' => 'your-merchant-salt',
 ];
