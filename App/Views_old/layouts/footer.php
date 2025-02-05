370f03081c1de20c64fc769356c302c787dfe62a
dk7458 <doriankrok@gmail.com>
2025-02-05 23:11:49 +0100

    hy

--- App/Views_old/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Path: App/Views/layouts/footer.php
-?>
-<footer class="footer">
-    &copy; <?= date("Y"); ?> Carfuse. Wszystkie prawa zastrzeżone. | <a href="/terms">Regulamin</a> | <a href="/privacy">Polityka prywatności</a>
-</footer>
