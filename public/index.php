1d1e43c3cbed5f2dfa862ebc3e8ac3a9a11fb2a6
dk7458 <doriankrok@gmail.com>
2025-02-06 19:49:20 +0100

    o

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . '/bootstrap.php'; // Ensure the bootstrap file is included
+require_once __DIR__ . '/../bootstrap.php'; // Ensure the bootstrap file is included
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
