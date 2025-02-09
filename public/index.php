f25073c53f339636878b6f85799b3132a193f3da
dk7458 <doriankrok@gmail.com>
2025-02-09 17:58:15 +0100

    i

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ startSecureSession();
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $logger->info("Requested URI: $requestUri");
 
-// ✅ PRIORITIZE API REQUEST HANDLING
+// ✅ API REQUEST HANDLING
 if (strpos($requestUri, '/api/') === 0) {
     $apiPath = $_SERVER['DOCUMENT_ROOT'] . $requestUri . '.php';
 
@@ -36,8 +36,9 @@ $routeInfo = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $requestUri);
 
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::FOUND:
+        // ✅ Handle Views in Subfolders
         $viewPath = __DIR__ . "/views/" . $routeInfo[1];
-        
+
         if (file_exists($viewPath)) {
             $logger->info("Rendering View: " . $routeInfo[1]);
             require $viewPath;
