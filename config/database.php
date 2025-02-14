1f968b4f64666d88f7df377164fe229a90c7f507
dk7458 <doriankrok@gmail.com>
2025-02-14 07:38:08 +0100

    u

---
+++ config/database.php
@@ -0,0 +1,22 @@
+return [
+    'app_database' => [
+        'driver'    => 'mysql',
+        'host'      => getenv('DB_HOST'),
+        'database'  => getenv('DB_DATABASE'),
+        'username'  => getenv('DB_USERNAME'),
+        'password'  => getenv('DB_PASSWORD'),
+        'charset'   => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix'    => '',
+    ],
+    'secure_database' => [
+        'driver'    => 'mysql',
+        'host'      => getenv('SECURE_DB_HOST'),
+        'database'  => getenv('SECURE_DB_DATABASE'),
+        'username'  => getenv('SECURE_DB_USERNAME'),
+        'password'  => getenv('SECURE_DB_PASSWORD'),
+        'charset'   => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix'    => '',
+    ]
+];
