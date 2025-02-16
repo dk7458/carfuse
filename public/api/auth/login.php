c28c25c9fd534ee1158b7c7fb1c56a7ee7e59eed
dk7458 <doriankrok@gmail.com>
2025-02-16 19:25:59 +0100

    i

--- public/api/auth/login.php
+++ public/api/auth/login.php
@@ -3,8 +3,6 @@
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../../App/Controllers/AuthController.php';
 
-use App\Controllers\AuthController;
-
 // ✅ Set Headers
 header('Content-Type: application/json');
 
