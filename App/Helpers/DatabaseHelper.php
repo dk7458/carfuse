4ba55d06f6dbec8909e3f861b7895be129fc6398
dk7458 <doriankrok@gmail.com>
2025-02-21 08:44:39 +0100

    k

--- App/Helpers/DatabaseHelper.php
+++ App/Helpers/DatabaseHelper.php
@@ -12,12 +12,12 @@ use function getLogger;
 
 class DatabaseHelper
 {
-    private static ?DatabaseHelper $instance = null;
-    private static ?DatabaseHelper $secureInstance = null;
-    private Capsule $capsule;
-    private static bool $envLoaded = false;
+    public static ?DatabaseHelper $instance = null;
+    public static ?DatabaseHelper $secureInstance = null;
+    public Capsule $capsule;
+    public static bool $envLoaded = false;
 
-    private function __construct(array $config)
+    public function __construct(array $config)
     {
         try {
             $this->capsule = new Capsule();
