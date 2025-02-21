c16844c7cc4eef80442528fe02a4879110adb160
dk7458 <doriankrok@gmail.com>
2025-02-21 07:45:02 +0100

    u

--- test_db.php
+++ test_db.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/debug.log');
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use App\Helpers\DatabaseHelper;
 
 header('Content-Type: application/json');
